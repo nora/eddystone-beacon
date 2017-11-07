7bfa9e2581f7b6e4dd21111369460ef165816e61
yir <app7th@gmail.com>
2017-11-07 15:18:46 +0900

    add package.json

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+//require
 var eb = require('eddystone-beacon');
-var url = "https://goo.gl/bdEL67";
-var options = {
+
+//URL
+const url = "https://goo.gl/bdEL67";
+//Options
+const options = {
   name: 'Beacon',    // set device name when advertising (Linux only)
   txPowerLevel: -22, // override TX Power Level, default value is -21,
   tlmCount: 2,       // 2 TLM frames
   tlmPeriod: 10      // every 10 advertisements
 };
 
+//set
 eb.advertiseUrl(url, options);
 
 console.log('eddystone-beacon is working.');
